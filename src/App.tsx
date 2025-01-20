2d39efea2f8c7024d969d4329a781ea4c6f855fb
MelR20 <melissa.raymond20@gmail.com>
2025-01-20 17:24:45 -0500

    fix : bug background scroll

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import './App.css'
 function App() {
   return (
     <>
-      <div className='background-container'>
       <video className='background' autoPlay loop muted playsInline >
         <source src="./public/background_loop_vid.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      <a href="https://www.vecteezy.com/free-videos/background">Background Stock Videos by Vecteezy</a>
-      </div>
+      <h1>Patate</h1>
+      <a className='BackgroundCredit' href="https://www.vecteezy.com/free-videos/background">Background Stock Videos by Vecteezy</a>
+
     </>
   )
 }
