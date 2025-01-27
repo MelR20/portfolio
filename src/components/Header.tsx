abae9deb5b524af338d76b8f0f55b92f544857f0
MelR20 <melissa.raymond20@gmail.com>
2025-01-27 18:49:42 -0500

    feat : header navbar base

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,20 @@
 
+const headerNavItems = [{name: 'À propos', link: '#aPropos' }, {name: 'Formations', link:'#formations'}, {name: 'Projets', link: '#projects'}, {name: 'Contact', link: '#contact'}];
 
 const Header = () => {
     return (
         <header className='header'>
-            <h1>Mélissa Raymond</h1>
+            <h1 className="logoName">Mélissa Raymond</h1>
+            <div style={{display: 'flex'}}>
+                <ul className = "headerNav">
+                    {headerNavItems.map((item, index) => (
 
+                        <li key={index} className="headerNavItem">
+                            <a href={item.link} className="headerNavItemLink">{item.name}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </header>
     );
 }
