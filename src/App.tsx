69fe1aebae50b4435a99652374f214e021a34207
MelR20 <melissa.raymond20@gmail.com>
2025-01-31 15:35:12 -0500

    refactor : header navbar refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
-import './styles/App.css'
-import Header from './components/Header'
+import "./styles/App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <video className='background' autoPlay loop muted playsInline >
+      <video className="background" autoPlay loop muted playsInline>
         <source src="/background_loop_vid.mp4" type="video/mp4" />
       </video>
-      <div className='mainContainer'>
-        <Header/>
-        <div className='contentContainer'>
+      <div className="main-container">
+        <Header />
+        <div className="content-container">
           <h1>Patate</h1>
           <h2>Patate</h2>
         </div>
       </div>
-      <a className='backgroundCredit' href="https://www.vecteezy.com/free-videos/background">Background Stock Videos by Vecteezy</a>
-
+      <a
+        className="background-credit"
+        href="https://www.vecteezy.com/free-videos/background"
+      >
+        Background Stock Videos by Vecteezy
+      </a>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,35 @@
-
-const headerNavItems = [{name: 'À propos', link: '#aPropos' }, {name: 'Formations', link:'#formations'}, {name: 'Projets', link: '#projects'}, {name: 'Contact', link: '#contact'}];
+const headerNavItems = [
+  { name: "À propos", link: "#aPropos" },
+  { name: "Formations", link: "#formations" },
+  { name: "Projets", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+];
 
 const Header = () => {
-    return (
-        <header className='header'>
-            <h1 className="logoName">Mélissa Raymond</h1>
-            <div style={{display: 'flex'}}>
-                <ul className = "headerNav">
-                    {headerNavItems.map((item, index) => (
-
-                        <li key={index} className="headerNavItem">
-                            <a href={item.link} className="headerNavItemLink">{item.name}</a>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </header>
-    );
-}
+  return (
+    <header className="header">
+      <div className="desktop-header">
+        <div className="desktop-header-grid">
+          <div className="header-logo">
+            <a href="/" className="header-logo-link">
+              <h1 className="logo-name">Mélissa Raymond</h1>
+            </a>
+          </div>
+          <div className="header-nav">
+            <ul className="header-nav-content">
+              {headerNavItems.map((item, index) => (
+                <li key={index} className="header-nav-item">
+                  <a href={item.link} className="header-nav-item-link">
+                    {item.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
