79fcbc70bce69cdeb42703b6b9c10d48eceb542c
MelR20 <melissa.raymond20@gmail.com>
2025-03-21 17:35:12 -0400

    chore: updated text in the A propos

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,19 @@ function App() {
         <Header />
         <div className="grid content-container">
           <div className="left-column">
-            <h1>Patate</h1>
+            <h1>À propos</h1>
+            <p>
+              Passionnée par la logique, les systèmes et les technologies, j’ai
+              fait le saut de la sexologie vers le développement logiciel.
+              Certifiée Scrum Master, j’ai d’abord exploré la programmation en
+              autodidacte avant de formaliser mes connaissances avec un
+              certificat en informatique à l’Université Laval. Mon parcours
+              atypique m’a permis d’acquérir une solide capacité d’analyse, une
+              approche méthodique de la résolution de problèmes et d’excellentes
+              compétences en collaboration et en communication. Aujourd’hui, je
+              mets ces atouts au service du développement logiciel, avec
+              l’ambition de créer des solutions efficaces et bien pensées.
+            </p>
           </div>
         </div>
       </div>
